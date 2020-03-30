a83a78e06164d1b925e3ae7e22e68552414eaa39
Omar Quiroz <omar@pragmateam.dev>
2020-03-30 23:50:33 +0200

    more small stuff:
    - populate tsconfig.json
    - add custom document file
    - add basic home page
    - add basic button component

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "./components";
+
+const Home = () => (
+  <>
+    <h1>Home page</h1>
+    <Button>Oh no</Button>
+  </>
+);
+
+export default Home;
