680464fa9add7cf31924618029c12892a76f38c0
Omar Quiroz <omar@pragmateam.dev>
2020-03-31 00:07:53 +0200

    feat: add basic home layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,29 @@ import { Button, Flex } from "../components";
 
 const Home = () => (
   <>
-    <h1>Home page</h1>
-    <Flex flexDirection="column">
-      <Button>Oh no</Button>
-      <Button>ah</Button>
-    </Flex>
+    <header>Im a header</header>
+    <main>
+      <section>
+        <h2>Slider</h2>
+        <p>Im a slider</p>
+      </section>
+      <section>
+        <h2>some other items</h2>
+        <p>Some other items oh aaaaa</p>
+      </section>
+      <section>
+        <h2>section</h2>
+        <p>dale a tu lpay</p>
+      </section>
+      <section>
+        <h2>Some buttons</h2>
+        <Flex flexDirection="column">
+          <Button>Oh no</Button>
+          <Button>ah</Button>
+        </Flex>
+      </section>
+    </main>
+    <footer>foooter footer</footer>
   </>
 );
 
